2da368d38fbdece0b218119b2e569b806eac9885
rixan1337 <pergodev@gmail.com>
2020-08-07 22:02:52 +0200

    2020.08.07 update

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+var _app = function () {
+    this.id = 0;
+    this.videoElement = null;
+    this.audioElement = null;
+    this.musicVolume = 0.12;
+    this.musicFadeIn = 4000;
+    this.skippedIntro = false;
+    this.backgroundToggler = false;
+    this.shouldIgnoreVideo = false;
+    this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
+    this.brandDescription = [
+        "paster",
+        "minecrafter",
+        "flyboys player",
+        "torrent master",
+        "hobby video cutter",
+        "hobby photographer"
+    ];
+
+    this.titleChanger = function (text, delay) {
+        text = text ||
+            ['.', 'r', 'ri', 'rix', 'rixa', 'rixan', 'rixa', 'rix', 'ri', 'r'];
+        delay = delay || 400;
+
+        var counter = 0;
+
+        setInterval(function () {
+            if(counter < text.length) 
+                document.title = text[counter++]; 
+            else 
+                document.title = text[counter = 0];
+        }, delay);
+    }
+};
+
+var app = new _app();
