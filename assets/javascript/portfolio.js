b35ebb60676c01c4e02e5b4925468121f43ad0f6
rixan1337 <pergodev@gmail.com>
2020-08-07 22:42:26 +0200

    too much fixes

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -43,9 +43,7 @@
     });
 
     $(window).on('keydown', function () {
-        if (event.keyCode == 123)
-            return false;
-        else if (event.ctrlKey && event.shiftKey && event.keyCode == 73)
+        if (event.ctrlKey && event.shiftKey && event.keyCode == 73)
             return false;
         else if (event.ctrlKey && event.keyCode == 73)
             return false;
